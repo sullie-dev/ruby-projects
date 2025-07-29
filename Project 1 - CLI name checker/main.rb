5238008b6c9cb34d2c25279bd919f5f1916a4652
Eoghan O'S <eoghan@sullie.dev>
2025-07-29 10:45:02 +0100

    Add vip_name array and an if statement to check if the user input is in the arrary

--- Project 1 - CLI name checker/main.rb
+++ Project 1 - CLI name checker/main.rb
@@ -1,11 +1,12 @@
-vip_name = "Eoghan"
+vip_names = ["Eoghan", "John", "Mary", "Mike"]
 
 print("What's you're name? ")
 
-user_string = gets.chomp
+user_string = gets.chomp.
 
-if vip_name == user_string
-    puts("Hey Eoghan you're on the list")
+
+ if vip_names.include?(user_string)
+    puts("Hey #{user_string} you're on the list")
 else
-    puts("You're not on the list")
+    puts("You're not on the list #{user_string}")
 end

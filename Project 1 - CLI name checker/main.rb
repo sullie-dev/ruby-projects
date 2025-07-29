ba9a899b76c56f74d56f24fb98a9498507a6505e
Eoghan O'S <eoghan@sullie.dev>
2025-07-29 10:34:04 +0100

    Add basic conditional logic

--- Project 1 - CLI name checker/main.rb
+++ Project 1 - CLI name checker/main.rb
@@ -1,5 +1,11 @@
+vip_name = "Eoghan"
+
 print("What's you're name? ")
 
 user_string = gets.chomp
 
-puts(user_string)
+if vip_name == user_string
+    puts("Hey Eoghan you're on the list")
+else
+    puts("You're not on the list")
+end

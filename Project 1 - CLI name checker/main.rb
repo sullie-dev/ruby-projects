fe99995b4976795760f43c945aa695e5d75e20d9
Eoghan O'S <eoghan@sullie.dev>
2025-07-29 11:18:12 +0100

    Fix syntax. Add Goodbye message

--- Project 1 - CLI name checker/main.rb
+++ Project 1 - CLI name checker/main.rb
@@ -1,20 +1,23 @@
 def check_list
     vip_names = ["Eoghan", "John", "Mary", "Mike"]
 
-    print("What's you're name? ")
+    print "What's your name? "
 
     user_string = gets.chomp.capitalize
 
+    if vip_names.include?(user_string)
+        puts "Hey #{user_string} you're on the list 🥳"
+    elsif user_string.downcase == "exit"
+        puts "Goodbye 👋"
+    else
+        puts "You're not on the list #{user_string} 😡"
+    end
+
     return :exit if user_string.downcase == "exit"
 
-    if vip_names.include?(user_string)
-        puts("Hey #{user_string} you're on the list")
-    else
-        puts("You're not on the list #{user_string}")
-    end
 end
 
 while true
-    break if check_list ==:exit
-    check_list()
+    result = check_list
+    break if result ==:exit
 end
